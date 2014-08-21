817070e87985a3624f331d1cb727489599ccaadb
Iraos <htjsouthern@gmail.com>
2014-08-21 01:25:57 +0100

    Functionality and Travis support

--- src/main/java/com/lgbteensmc/profilepluginv3/Main.java
+++ src/main/java/com/lgbteensmc/profilepluginv3/Main.java
@@ -1,7 +1,35 @@
 package com.lgbteensmc.profilepluginv3;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public final class profilepluginv3 extends JavaPlugin {
-	 
+public final class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    	getLogger().info("ProfilePluginV3 is loaded!");
+    }
+ 
+    @Override
+    public void onDisable() {
+    	getLogger().info("ProfilePluginV3 has been unloaded.");
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("basic2")) {
+    		if (!(sender instanceof Player)) {
+    			sender.sendMessage("This command can only be run by a player.");
+    		} else {
+    			Player player = (Player) sender;
+    			sender.sendMessage("Setprofile running");
+    		}
+    		return true;
+    	}
+    	return false;
+    }
+    
+    
+    
 }
